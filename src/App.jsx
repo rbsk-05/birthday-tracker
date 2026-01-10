ae975af8f8742739d06129851efa34190dc52449
Darshan-M05 <230701061@rajalakshmi.edu.in>
2026-01-10 13:34:34 +0530

    Initial Layout Setup desgined

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App-header">
+      <h1>Birthday Tracker</h1>
+    </div>
+
+    <div className="App-content">
+
+      <div className="Upcoming-div">
+        <h3>Upcoming BirthDays</h3>
+
+        <div className="Birthday-card-div">
+            <div className='Birthday-card'>
+              <div className="Birthday-card-header">
+                <span className='Birthday-tag'>Friend</span>
+                <span className='Birthday-days'>10 days</span>
+              </div>
+
+              <div className='Birthday-content-div'>
+                <img className="Birthday-img" src="darshan.jpg" />
+              <p className="Birthday-name">DARSHAN M</p>
+              <p className="Birthday-date">MAY 18, WED</p>
+              <p className="Birthday-age">AGE: 21</p>
+              </div>
+            </div>
+
+            <div className='Birthday-card'>
+              <div className="Birthday-card-header">
+                <span className='Birthday-tag'>Friend</span>
+                <span className='Birthday-days'>10 days</span>
+              </div>
+
+              <div className='Birthday-content-div'>
+                <img className="Birthday-img" src="darshan.jpg" />
+              <p className="Birthday-name">DARSHAN M</p>
+              <p className="Birthday-date">MAY 18, WED</p>
+              <p className="Birthday-age">AGE: 21</p>
+              </div>
+            </div>
+
+        </div>
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="Recent-div">
+        <h3>Recent Birthdays</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
