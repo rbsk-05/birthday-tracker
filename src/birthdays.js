1f4516496e66278437381e2984bfcf24d69d45e6
Darshan-M05 <230701061@rajalakshmi.edu.in>
2026-01-17 09:31:18 +0530

    Entry of DataBase

--- src/birthdays.js
+++ src/birthdays.js
@@ -3,43 +3,225 @@ const birthdays = [
     id : 1,
     name : "Darshan M",
     dob : "May 18 2005",
-    tag : "Friend",
-    image: "darshan.jpg"
+    tag : "Family",
+    image: "d4.jpg"
   },
   {
     id : 2,
     name : "RBSK",
     dob : "Jan 1 2006",
-    tag : "Friend",
-    image : "darshan.jpg"
+    tag : "Family",
+    image : "profile.jpg"
   },
   {
     id : 3,
-    name : "Rekha",
+    name : "Mom",
     dob : "April 10 1981",
-    tag : "Friend",
-    image : "darshan.jpg"
+    tag : "Family",
+    image : "mummy.jpg"
   },
   {
     id : 4,
-    name : "Murugan",
+    name : "Dad",
     dob : "Oct 18 1976",
-    tag : "Friend",
-    image : "darshan.jpg"
+    tag : "Family",
+    image : "daddy.jpg"
   },
   {
     id : 5,
-    name : "Darshan M",
-    dob : "Jan 15 2005",
+    name : "AK",
+    dob : "Jul 21 2005",
     tag : "Friend",
-    image: "darshan.jpg"
+    image: "ak.jpg"
   },
   {
     id : 6,
-    name : "Darshan M",
-    dob : "Feb 13 2005",
+    name : "Aakash",
+    dob : "Oct 5 2005",
     tag : "Friend",
-    image: "darshan.jpg"
+    image: "aakash.jpg"
+  },
+  {
+    id : 7,
+    name : "Janane S",
+    dob : "April 30 2005",
+    tag : "Friend",
+    image: "janane.jpg"
+  },
+  {
+    id : 8,
+    name : "Akshatta",
+    dob : "April 12 2006",
+    tag : "Friend",
+    image: "aks.jpg"
+  },
+  {
+    id : 9,
+    name : "Sanjith G",
+    dob : "Oct 16 2005",
+    tag : "Friend",
+    image: "g.jpg"
+  },
+  {
+    id : 10,
+    name : "Vishnu",
+    dob : "Oct 14 2005",
+    tag : "Friend",
+    image: "vishnu.jpg"
+  },
+  {
+    id : 11,
+    name : "Dhivya Shree",
+    dob : "Sep 9 2005",
+    tag : "Friend",
+    image: "profile.jpg"
+  },
+  {
+    id : 12,
+    name : "Sethuram",
+    dob : "Mar 3 2005",
+    tag : "Friend",
+    image: "sethuram.jpg"
+  },
+  {
+    id : 13,
+    name : "MC Arun",
+    dob : "Jan 10 2005",
+    tag : "College",
+    image: "mc.jpg"
+  },
+  {
+    id : 14,
+    name : "Deepak K",
+    dob : "Jun 5 2006",
+    tag : "College",
+    image: "deepak.jpg"
+  },
+  {
+    id : 15,
+    name : "Koushik S",
+    dob : "April 16 2006",
+    tag : "College",
+    image: "koushik.jpg"
+  },
+  {
+    id : 16,
+    name : "Aravinthaa S",
+    dob : "May 9 2005",
+    tag : "College",
+    image: "ara.jpg"
+  },
+  {
+    id : 16,
+    name : "Akshay Kumar S",
+    dob : "Jun 23 2006",
+    tag : "College",
+    image: "akshay.jpg"
+  },
+  {
+    id : 17,
+    name : "Bharath Kumar S",
+    dob : "Oct 10 2005",
+    tag : "College",
+    image: "bk.jpg"
+  },
+  {
+    id : 18,
+    name : "Bernieo Chan",
+    dob : "Oct 5 2005",
+    tag : "College",
+    image: "chan.jpg"
+  },
+  {
+    id : 19,
+    name : "Ashish",
+    dob : "April 6 2006",
+    tag : "College",
+    image: "ashish2.jpg"
+  },
+  {
+    id : 20,
+    name : "Alfred Sam",
+    dob : "July 6 2006",
+    tag : "College",
+    image: "sam.jpg"
+  },
+  {
+    id : 21,
+    name : "Daniel Leve",
+    dob : "Aug 25 2005",
+    tag : "College",
+    image: "profile.jpg"
+  },
+  {
+    id : 22,
+    name : "Brijith",
+    dob : "July 30 2006",
+    tag : "College",
+    image: "brijith.jpg"
+  },
+  {
+    id : 23,
+    name : "Aadhithiya",
+    dob : "July 12 2005",
+    tag : "College",
+    image: "aadhi.jpg"
+  },
+  {
+    id : 24,
+    name : "Shanjiv Krishna",
+    dob : "July 12 2005",
+    tag : "Friend",
+    image: "shanjiv.jpg"
+  },
+  {
+    id : 25,
+    name : "Poorani",
+    dob : "Nov 28 2011",
+    tag : "Friend",
+    image: "profile.jpg"
+  },
+  {
+    id : 26,
+    name : "Arunachalam N",
+    dob : "Oct 20 2005",
+    tag : "Friend",
+    image: "aruna.jpg"
+  },
+  {
+    id : 27,
+    name : "Shaameer",
+    dob : "Jan 13 2006",
+    tag : "Friend",
+    image: "shaameer.jpg"
+  },
+  {
+    id : 28,
+    name : "Vijay Sekar S",
+    dob : "Nov 21 2005",
+    tag : "Friend",
+    image: "vijay.jpg"
+  },
+  {
+    id : 29,
+    name : "Shanmuganathan",
+    dob : "Jan 15 2005",
+    tag : "College",
+    image: "shan.jpg"
+  },
+  {
+    id : 30,
+    name : "Dayanidhi",
+    dob : "Sep 13 2005",
+    tag : "College",
+    image: "daya.jpg"
+  },
+  {
+    id : 31,
+    name : "Gian",
+    dob : "Jun 19 2006",
+    tag : "College",
+    image: "gian.jpg"
   }
 ];
 
