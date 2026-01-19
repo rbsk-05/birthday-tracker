abd33b2d0b919ae6c04dd4c624a6fc7729dbf442
Darshan-M05 <230701061@rajalakshmi.edu.in>
2026-01-19 17:45:34 +0530

    GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/birthday-tracker/', //Setting up the BASE PATH FOR GITHUB PAGES
 })
